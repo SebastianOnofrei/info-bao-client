1fdfd80c192b11b639ac6923b7ce2874a6e58531
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-02-19 00:18:11 +0200

    feat: added boilerplate for the other routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Home = lazy(() => import("./components/pages/Home/Home"));
+const About = lazy(() => import("./components/pages/About/About"));
+const Blog = lazy(() => import("./components/pages/Blog/Blog"));
+const Contact = lazy(() => import("./components/pages/Contact/Contact"));
+const Cosmetics = lazy(() => import("./components/pages/Cosmetics/Cosmetics"));
+const Product = lazy(() => import("./components/pages/Product/Product"));
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cosmetics" element={<Cosmetics />} />
+          <Route path="/product" element={<Product />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
---
+++ src/components/pages/Cosmetics/Cosmetics.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cosmetics = () => {
+  return <div>Cosmetics</div>;
+};
+
+export default Cosmetics;
