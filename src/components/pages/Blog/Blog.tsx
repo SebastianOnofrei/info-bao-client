bd6f9b54043d0c24da493166ae3137bb7ea48fe3
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-02-21 13:29:47 +0200

    feat: made preliminary blog list page.. 80% done

--- src/components/pages/Blog/Blog.tsx
+++ src/components/pages/Blog/Blog.tsx
@@ -1,5 +1,114 @@
+import "./Blog.css";
+
+import bodylotion from "../../../assets/bodylotion.png";
+import bodyoil from "../../../assets/bodyoil.png";
+import bodyscrab from "../../../assets/bodyscrab.png";
+
+import cleanser from "../../../assets/cleanser.png";
+import eyepatches from "../../../assets/eyepatches.png";
+import mask from "../../../assets/mask.png";
+
+import hairbrush from "../../../assets/hairbrush.png";
+import hairstyling from "../../../assets/hairstyling.png";
+import splitends from "../../../assets/splitends.png";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  const faceBlogs = [
+    {
+      imgUrl: cleanser,
+      title: "Cleansers",
+    },
+    {
+      imgUrl: eyepatches,
+      title: "Eye Patches",
+    },
+    {
+      imgUrl: mask,
+      title: "Masks",
+    },
+  ];
+
+  const bodyBlogs = [
+    {
+      imgUrl: bodyscrab,
+      title: "Body Scrab",
+    },
+    {
+      imgUrl: bodylotion,
+      title: "Body Lotion",
+    },
+    {
+      imgUrl: bodyoil,
+      title: "Body Oil",
+    },
+  ];
+
+  const hairBlogs = [
+    {
+      imgUrl: hairbrush,
+      title: "Hairbrush",
+    },
+    {
+      imgUrl: hairstyling,
+      title: "Hair Styling",
+    },
+    {
+      imgUrl: splitends,
+      title: "Split Ends",
+    },
+  ];
+
+  return (
+    <div className="blogs-container">
+      <h3>Blogs</h3>
+      <div className="category-container">
+        <h3>Face</h3>
+        <div className="blogs-flex-container">
+          {faceBlogs.map((blog) => {
+            return (
+              <div>
+                <span>{blog.title}</span>
+                <img src={blog.imgUrl} alt="" />
+                <a>More</a>
+              </div>
+            );
+          })}
+        </div>
+        <button>More</button>
+      </div>
+      <div className="category-container">
+        <h3>Body</h3>
+        <div className="blogs-flex-container">
+          {bodyBlogs.map((blog) => {
+            return (
+              <div>
+                <span>{blog.title}</span>
+                <img src={blog.imgUrl} alt="" />
+
+                <a>More</a>
+              </div>
+            );
+          })}
+        </div>
+        <button>More</button>
+      </div>
+      <div className="category-container">
+        <h3>Hair</h3>
+        <div className="blogs-flex-container">
+          {hairBlogs.map((blog) => {
+            return (
+              <div>
+                <span>{blog.title}</span>
+                <img src={blog.imgUrl} alt={blog.title} />
+                <a>More</a>
+              </div>
+            );
+          })}
+        </div>
+        <button>More</button>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
