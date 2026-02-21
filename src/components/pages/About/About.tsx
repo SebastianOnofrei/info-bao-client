b9818091ddaad9077c926d4954a704694e16c2c2
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-02-21 12:42:08 +0200

    feat: created About us page, for desktop view

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,5 +1,16 @@
+import { ABOUT } from "../../../constants";
+import "./About.css";
+
 const About = () => {
-  return <div>aBOUT</div>;
+  return (
+    <div className="about-container">
+      <div>
+        <h3>{ABOUT.TITLE}</h3>
+        <p>{ABOUT.DESCRIPTION}</p>
+        <p>{ABOUT.LOVE}</p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,10 @@ export const FOOTER = {
   PHONE_NUMBER: "+7542940022",
   PUNCHLINE: "We always ready to speak with you!",
 };
+
+export const ABOUT = {
+  TITLE: "About us",
+  DESCRIPTION:
+    "In the main page you can read about us. But here you can read more about project. And what we want do on this site. All women want look beautiful but they can afraid.  Firstly they can afraid because they don't know how start. Secondly they don't know how right apply makeup that's why we want publish different video about makeup. I think it's useful information. And finally you can find information about different brands. And now I can tell about myself. My name Julia. I have eiughteen years old. Yes I know now you think how this young girl can talk to us about problem skin. When she has got clean skin and young skin. But I have got problem skin. I have got acne, acne scars and acne on body. I want so that this site been for you good informant about cosmetics. Here you can read only comments different people  about cosmetics or composition and information about different brands product.   But you need remember that all people have different skin. What suits some does not suit others. When you buy new cosmetics you need always tested on your arm before applying on face. It's very important because you can prevent  allergy on product components.  Always remember about that all women can have got clean and beautiful skin but if you want it.  ",
+  LOVE: "We love you!",
+};
