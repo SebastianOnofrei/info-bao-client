f60ab5d7ca4661f124eb635873525b0f9dea7640
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-02-23 20:49:01 +0200

    feat: added contact bare minimum

--- src/components/pages/Contact/Contact.tsx
+++ src/components/pages/Contact/Contact.tsx
@@ -1,5 +1,17 @@
+import "./Contact.css";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-container">
+      <div>
+        <h3>CONTACT</h3>
+        <p>Phone number: +1234567890 </p>
+        <p>Email: infobao-contact@gmail.com</p>
+        <p>Location: South Los Angeles </p>
+        <p>Language: Russian, English, Ukrainian, Spanish.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
