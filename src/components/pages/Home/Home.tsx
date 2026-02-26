4ab386ad532158cd938065b6ed973a2904ad9e54
Sebastian Onofrei <sebastian.onofrei.business@gmail.com>
2026-02-26 19:57:22 +0200

    feat: created a more polished version of home, with images, and 2 sections

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,6 +3,8 @@ import "./Home.css";
 import { homeVariants } from "../../../animations/transitions";
 import Footer from "../../organisms/Footer/Footer";
 import Nav from "../../organisms/Nav/Nav";
+import heroImg from "../../../assets/home.png";
+import homeAboutImg from "../../../assets/about-home.png";
 
 const Home = () => {
   return (
@@ -13,33 +15,48 @@ const Home = () => {
       exit="exit"
       className="home-container"
     >
-      <Nav />
-      <section className="home-section">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
-          asperiores natus molestiae nesciunt ad. Odit quae dolorem fugiat
-          repellat distinctio quis, facere quasi, veniam tenetur aperiam vero
-          laudantium deleniti rerum sit blanditiis natus consectetur autem est
-          iusto minus, numquam eius eveniet temporibus ipsum? Excepturi ullam
-          iure omnis nihil dolores veritatis ratione pariatur modi, voluptas
-          possimus aut blanditiis repudiandae itaque voluptatem libero est
-          beatae assumenda! Neque non, obcaecati, veniam consequuntur aut magni
-          consequatur illum, voluptatibus blanditiis nemo culpa molestias
-          debitis. Dolore, illum quos vel sint labore placeat tempore. Quibusdam
-          eos tempore cumque consequatur ipsa beatae temporibus nobis quaerat
-          sunt quia excepturi molestias reiciendis rem earum provident natus,
-          consectetur esse minus sapiente illo neque pariatur tenetur corporis
-          aliquid? Laudantium perspiciatis officiis quis voluptatum nemo
-          inventore error impedit corporis? Aliquid, velit vel sequi unde
-          mollitia id voluptates ullam labore quod repellat illum corrupti rerum
-          magni harum in eius, error dicta, officia deserunt iure dignissimos!
-          Dolorum accusantium voluptate, harum asperiores consequuntur deleniti
-          ab voluptatem! Odit deserunt natus voluptatem laboriosam temporibus
-          asperiores doloribus pariatur id maxime libero, et aut beatae
-          voluptate deleniti nihil ex quasi cum debitis doloremque quos expedita
-          tenetur. Quibusdam, doloribus quo asperiores officia quisquam
-          voluptate vel blanditiis placeat provident, in natus error.
-        </p>
+      <div className=""></div>
+
+      <section className="hero">
+        <img src={heroImg} alt="Hero Background" className="hero-img" />
+
+        <div className="nav-container">
+          <Nav />
+        </div>
+
+        <div className="hero-content">
+          <h1>About Cosmetics</h1>
+          <p>Learn more about other cosmetics with INFO BAO.</p>
+          <button>More info</button>
+        </div>
+      </section>
+
+      <section className="hero-about-section">
+        <h2>About Us</h2>
+        <div className="hero-about-content">
+          <div>
+            <h3>start new info</h3>
+            <p>
+              When you want buy new cosmetics you need know about this
+              cosmetics. This site help you find cosmetics what you want. We can
+              help you to get acquainted with cosmetics. And you can find
+              information about skin problem for teenager, adult and elder
+              woman. We tell you about face skin cosmetics, hair cosmetics,
+              cosmetics for makeup. We have got three sections: Korean
+              cosmetics, American cosmetics, Europian cosmetics.
+            </p>
+
+            <p>
+              We talk about brands: CHI(American hair cosmetics), Some by
+              mi(Korean face cosmetics), La Roche-Posay(French face cosmetics)
+              and other brands. We take cosmetics different price. Because not
+              all can buy expensive cosmetics but all women want look beautiful.
+              That's why we start this project! All women can buy cosmetics!
+            </p>
+          </div>
+          <img src={homeAboutImg} alt="home About us image" />
+        </div>
+        <button>More</button>
       </section>
 
       <Footer />
